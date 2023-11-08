762aa003e40f4d9331710b49bee5cf3f9e8072a1
alex <al3xback@gmail.com>
2023-11-08 14:00:01 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fmentor-article-preview-angular-base-services-multi';
+}
